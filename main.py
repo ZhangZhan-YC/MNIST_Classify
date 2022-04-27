a78477f0b94e7d94873a247990e17daf1316a4f0
MatthewZZ <ZhangZhanYC@163.com>
2022-04-27 20:37:01 +0800

    third commit(GPU)

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import MnistDataLoader
 from os.path import join
-import numpy as np
+import time
 
-# TODO：优化正确率
 # 搭建网络
 class Net(torch.nn.Module):
     def __init__(self, n_feature, n_hidden1, n_hidden2, n_output):
@@ -43,26 +42,48 @@ mnist_dataloader = MnistDataLoader.MnistDataloader(training_images_filepath, tra
 (x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
 # print(np.array(x_train).shape)
 
-x_train = torch.tensor(x_train).float()
-y_train = torch.tensor(y_train)
-x_test = torch.tensor(x_test).float()
-y_test = torch.tensor(y_test)
-print(x_train.size(), y_train.size())
+# 处理数据并将数据塞进GPU
+data_train = torch.tensor(x_train).float()
+label_train = torch.tensor(y_train)
+x_train = data_train.cuda()
+y_train = label_train.cuda()
+print('x_train:', x_train.device)
+print('y_train:', x_train.device)
+data_test = torch.tensor(x_test).float()
+label_test = torch.tensor(y_test)
+x_test = data_test.cuda()
+y_test = label_test.cuda()
+print('x_test:', x_test.device)
+print('y_test:', y_test.device)
+# print(x_train.size(), y_train.size())
 # print(y_train[0].type())
 # print(x_train[0].type())
 # print(x_train[0, 0].type())
 
 # 训练
-num_epochs = 500
+num_epochs = 10000
+loss_old = 0
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# 把模型塞进GPu
+net.to(device)
+time_start = time.time()
 for t in range(num_epochs):
     prediction = net(x_train)
-    print(prediction.argmax(dim=-1))
+    # print(prediction.argmax(dim=-1))
     loss = loss_func(prediction, y_train)
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+    if torch.abs(loss_old - loss)< 1e-6:
+        break
     # print(f'number of feature:{i + 1}  loss:{loss}')
-    print(f'epoch:{t+1}  loss:{loss}')
+    if (t+1) % 100 == 0:
+        print(f'epoch:{t+1}  loss:{loss}')
+    loss_old = loss
+time_end = time.time()
+print(f'time of training:{time_end - time_start}')
+
+# TODO 绘制损失下降图像
 
 # 验证
 prediction = net(x_test)
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-import torch as t
-from torchvision import datasets
+# import torch as t
+# from torchvision import datasets
+#
+# data_train = datasets.MNIST(root="./data", train=True, download=True)
+# data_test = datasets.MNIST(root="./data", train=False)
 
-data_train = datasets.MNIST(root="./data", train=True, download=True)
-data_test = datasets.MNIST(root="./data", train=False)
-
+import torch
+from torch import nn
+#  查看gpu信息
+cudaMsg = torch.cuda.is_available()
+gpuCount = torch.cuda.device_count()
+print("1.是否存在GPU:{}".format(cudaMsg), "如果存在有：{}个".format(gpuCount))
